35ccaf815a5631d8610b5aa6d2bdd38d179e1c5a
Tolo <tololima.auvaa@gmail.com>
2022-12-13 05:03:36 -0600

    added author box

--- frontend/src/SubmitBox/SubmitBox.js
+++ frontend/src/SubmitBox/SubmitBox.js
@@ -5,14 +5,23 @@ import React from "react";
 
 const SubmitBox = () => {
     const [subtext, setSubtext] = React.useState("submit box");
+    const [author, setAuthor] = React.useState("anon");
     const handleSubmit = () => {
-        let mes = new message(subtext);
+        let mes = new message(subtext, author);
         let toSend = mes.makeMessage();
         SendMessage(toSend);
     };
     return (
         <div className="SubmitBox">
             <form onSubmit={handleSubmit}>
+                <div className="author">
+                    <label for="author">Author:</label>
+                    <input
+                        id="author"
+                        type={"text"}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    ></input>
+                </div>
                 <textarea
                     onChange={(e) => setSubtext(e.target.value)}
                 ></textarea>
